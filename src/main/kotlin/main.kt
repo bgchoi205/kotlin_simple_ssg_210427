d2589db2873824081e0d930967b8c5aeb4455399
bgchoi205 <choibg205@naver.com>
2021-04-27 12:14:53 +0900

    default

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,6 @@
+import java.lang.NullPointerException
+import java.lang.NumberFormatException
+
 fun main(){
 
     println("simple ssg 시작")
@@ -6,36 +9,97 @@ fun main(){
 
         print("명령어 : ")
         val command = readLine()!!.trim()
-        val commandBits = command.split("?", limit = 2)
-        val url = commandBits[0]
-        var paramStr = commandBits[1]
-        val paramMap = mutableMapOf<String, String>()
 
-        val paramStrBits = paramStr.split("&")
+        val rq = Rq(command)
 
-        for(paramStrBit in paramStrBits){
-            val paramStrBitBits = paramStrBit.split("=", limit = 2)
-            val key = paramStrBitBits[0]
-            val value = paramStrBitBits[1]
-
-            paramMap[key] = value
-        }
-
-        when(url) {
-            "/system/exit" -> {
-                println("프로그램 종료")
-                break
-            }
-            "/article/detail" -> {
-                val id = paramMap["id"]!!.toInt()
-
-
-            }
-
-        }
+//        println(rq.getStringParam("title", "1") == "1") // true
+//        println(rq.getIntParam("id", -1) == -1) // true
 
     }
 
     println("simple ssg 끝")
 
 }
+
+class Rq(command:String){
+
+    val actionPath : String
+    val paramMap : Map<String, String>
+
+    init{
+        val commandBits = command.split("?", limit = 2)
+
+        actionPath = commandBits[0].trim()
+
+        val queryStr = if(commandBits.lastIndex == 1 && commandBits[1].isNotEmpty()){
+            commandBits[1].trim()
+        }else {
+            ""
+        }
+
+        paramMap = if (queryStr.isEmpty()) {
+            mapOf()
+        } else {
+            val paramMapTemp = mutableMapOf<String, String>()
+
+            // queryStr = id=1&body=2&title=3&age
+            val queryStrBits = queryStr.split("&")
+
+            for (queryStrBit in queryStrBits) {
+                // queryStrBit = id=1
+                val queryStrBitBits = queryStrBit.split("=", limit = 2)
+                val paramName = queryStrBitBits[0]
+                val paramValue = if (queryStrBitBits.lastIndex == 1 && queryStrBitBits[1].isNotEmpty()) {
+                    queryStrBitBits[1].trim()
+                } else {
+                    ""
+                }
+
+                if (paramValue.isNotEmpty()) {
+                    paramMapTemp[paramName] = paramValue
+                }
+            }
+
+            paramMapTemp.toMap()
+        }
+    }
+
+    fun getStringParam(name : String, default : String): String {
+
+        return paramMap[name] ?: default
+
+
+//        return if(paramMap[name] == null){
+//            default
+//        }
+//        else{
+//            paramMap[name]!!
+//        }
+
+
+//        return try{
+//            paramMap[name]!!
+//        }
+//        catch(e:NullPointerException){
+//            default
+//        }
+    }
+
+    fun getIntParam(name : String, default : Int): Int {
+//        return  paramMap[name]!!.toInt() ?: default
+
+        return if(paramMap[name] != null){
+            try{
+            paramMap[name]!!.toInt()
+            }
+            catch(e : NumberFormatException){
+                default
+            }
+        }
+        else{
+            default
+        }
+    }
+
+}
+
